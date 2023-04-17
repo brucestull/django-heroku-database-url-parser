d80d6bbe329721c77630e846eaa94f5db199140a
Bruce Stull <bruce.stull@gmail.com>
2023-04-17 05:10:15 -0400

    Second attempt at my solution

--- database_url_parser.py
+++ database_url_parser.py
@@ -16,13 +16,27 @@ def get_database_config_variables(url):
     """
     # Remove the `postgres://` from the beginning of the string
     url = url.split('://')[1]
-    # Split the string into a list of strings at the `:` delimeter
-    url = url.split(':')
-    # Split the second string url[1] into a list of strings at the `@` delimeter
-    url[1] = url[1].split('@')
-    # Split the third string url[2] into a list of strings at the `/` delimeter
-    url[2] = url[2].split('/')
-
-    return url
+    # Split the remaining string into a list on the `@` character, which separates the database credentials from the host info
+    credentials_and_host_info = url.split('@')
+    # Get the database credentials (`DATABASE_USER` and `DATABASE_PASSWORD`) from the first item in the list
+    credentials = credentials_and_host_info[0].split(':')
+    # Get the database `host_info` from the second item in the list, which is the `DATABASE_HOST`, `DATABASE_PORT`, and `DATABASE_NAME`
+    host_info = credentials_and_host_info[1].split(':')
+    # Get the database host and port from the first item in the list
+    host = host_info[0]
+    # Get the database port and name from the second item in the list
+    port_and_name = host_info[1].split('/')
+    # Get the database port and name from the first item in the list
+    port = port_and_name[0]
+    # Get the database name from the second item in the list
+    name = port_and_name[1]
+    # Return a dictionary with the database credentials and host info
+    return {
+        'DATABASE_USER': credentials[0],
+        'DATABASE_PASSWORD': credentials[1],
+        'DATABASE_HOST': host,
+        'DATABASE_PORT': port,
+        'DATABASE_NAME': name
+    }
 
 print(get_database_config_variables(the_test_url))
